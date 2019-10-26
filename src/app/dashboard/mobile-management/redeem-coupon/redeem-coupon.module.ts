6ce0df5a639f0edec2863b178460e18a47b80c18
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2019-10-26 15:30:55 -0300

    Mobile: Agregada librería para escanear QR

--- src/app/dashboard/mobile-management/redeem-coupon/redeem-coupon.module.ts
+++ src/app/dashboard/mobile-management/redeem-coupon/redeem-coupon.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
+import { ZXingScannerModule } from "@zxing/ngx-scanner";
 import { RedeemCouponRoutingModule } from "./redeem-coupon-routing.module";
 import { RedeemCouponComponent } from "@app/dashboard/mobile-management/redeem-coupon/redeem-coupon.component";
 
 @NgModule({
   declarations: [RedeemCouponComponent],
-  imports: [CommonModule, RedeemCouponRoutingModule]
+  imports: [CommonModule, RedeemCouponRoutingModule, ZXingScannerModule]
 })
 export class RedeemCouponModule {}
